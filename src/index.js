d53c47f7087f8d0c76b605e6589a0b01d2320d90
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2019-11-06 19:34:47 -0300

    Handle android case

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-import { NativeModules, NativeEventEmitter } from 'react-native';
+import { NativeModules, NativeEventEmitter, Platform } from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
 
 const { KeyCommandConstants, KeyCommandsManager } = NativeModules;
 export const constants = {
-  keyModifierShift: KeyCommandConstants.keyModifierShift,
-  keyModifierControl: KeyCommandConstants.keyModifierControl,
-  keyModifierAlternate: KeyCommandConstants.keyModifierAlternate,
-  keyModifierCommand: KeyCommandConstants.keyModifierCommand,
-  keyModifierNumericPad: KeyCommandConstants.keyModifierNumericPad,
-  keyInputUpArrow: KeyCommandConstants.keyInputUpArrow,
-  keyInputDownArrow: KeyCommandConstants.keyInputDownArrow,
-  keyInputLeftArrow: KeyCommandConstants.keyInputLeftArrow,
-  keyInputRightArrow: KeyCommandConstants.keyInputRightArrow,
-  keyInputEscape: KeyCommandConstants.keyInputEscape,
+  keyModifierShift: !isAndroid ? KeyCommandConstants.keyModifierShift : null,
+  keyModifierControl: !isAndroid ? KeyCommandConstants.keyModifierControl : null,
+  keyModifierAlternate: !isAndroid ? KeyCommandConstants.keyModifierAlternate : null,
+  keyModifierCommand: !isAndroid ? KeyCommandConstants.keyModifierCommand : null,
+  keyModifierNumericPad: !isAndroid ? KeyCommandConstants.keyModifierNumericPad : null,
+  keyInputUpArrow: !isAndroid ? KeyCommandConstants.keyInputUpArrow : null,
+  keyInputDownArrow: !isAndroid ? KeyCommandConstants.keyInputDownArrow : null,
+  keyInputLeftArrow: !isAndroid ? KeyCommandConstants.keyInputLeftArrow : null,
+  keyInputRightArrow: !isAndroid ? KeyCommandConstants.keyInputRightArrow : null,
+  keyInputEscape: !isAndroid ? KeyCommandConstants.keyInputEscape : null,
 };
 
-export const KeyCommandsEmitter = new NativeEventEmitter(KeyCommandsManager);
+export const KeyCommandsEmitter = !isAndroid ? new NativeEventEmitter(KeyCommandsManager) : {
+  addListener: () => {}
+};
 
-export default KeyCommandsManager;
+export default !isAndroid ? KeyCommandsManager : {
+  setKeyCommands: () => {},
+  removeKeyCommands: () => {}
+};
